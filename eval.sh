400a65497c510347cab5f934b858b5e38a3986df
Nils Imdahl <imdahlnils@gmail.com>
2025-06-12 14:45:57 +0200

    use gpu for eval

--- eval.sh
+++ eval.sh
@@ -1,8 +1,8 @@
-fairseq-generate ./dataset/fairseq \
-  --path checkpoints/sorbian_german_transformer/checkpoint_last.pt \
-  --batch-size 128 \
-  --beam 5 \
-  --remove-bpe \
-  --source-lang hsb \
-  --arch transformer \
-  --target-lang de
+CUDA_VISIBLE_DEVICES=1 fairseq-generate ./dataset/fairseq \
+    --path checkpoints/sorbian_german_transformer/checkpoint_best.pt \
+    --batch-size 128 \
+    --beam 5 \
+    --remove-bpe \
+    --source-lang hsb \
+    --target-lang de \
+    --arch transformer_iwslt_de_en
